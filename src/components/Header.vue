e76a893464c43a6ff4dd17fc64c0cc154f095afd
Movsumlu Tabriz <tabrizmovsumlu@MacBook-Pro-Tabriz.local>
2021-07-01 13:07:09 +0300

    refactoring css-styles in components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -148,3 +148,248 @@ export default {
   name: "Header",
 };
 </script>
+
+<style lang="scss" scoped>
+.sort-form {
+  display: flex;
+}
+
+.sort-form form {
+  display: flex;
+  min-width: 712px;
+  padding: 12px 20px;
+
+  background-color: var(--color-blue-main);
+}
+
+.sort-form__group {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: start;
+  flex-shrink: 0;
+  margin-right: 20px;
+  margin-bottom: -2px;
+}
+
+.sort-form__group:last-child {
+  margin-right: 0;
+}
+
+.sort-form__group > div {
+  margin-right: 2px;
+  margin-bottom: 2px;
+}
+
+.sort-form__group > div:last-child {
+  margin-right: 0;
+}
+
+.sort-form__group > div:last-child {
+  margin-right: 0;
+}
+
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  width: 24px;
+  height: 24px;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+}
+
+.icon--arrow-down {
+  width: 20px;
+  height: 22px;
+
+  background-image: url("../assets/img/icons/icon-arrow-down.svg");
+}
+
+.icon--arrow-up {
+  width: 20px;
+  height: 22px;
+
+  background-image: url("../assets/img/icons/icon-arrow-up.svg");
+}
+
+.icon--strips-small {
+  width: 6px;
+  height: 16px;
+
+  background-image: url("../assets/img/icons/icon-strips-small.svg");
+}
+
+.icon--strips-big {
+  width: 6px;
+  height: 22px;
+
+  background-image: url("../assets/img/icons/icon-strips-big.svg");
+}
+
+.icon--rainy {
+  background-image: url("../assets/img/icons/icon-rainy.svg");
+}
+
+.icon--sunny {
+  background-image: url("../assets/img/icons/icon-sunny.svg");
+}
+
+.icon--cloudy {
+  background-image: url("../assets/img/icons/icon-cloudy.svg");
+}
+
+.icon--snowy {
+  background-image: url("../assets/img/icons/icon-snowy.svg");
+}
+
+.icon--stormy {
+  background-image: url("../assets/img/icons/icon-stormy.svg");
+}
+
+.icon--blizzard {
+  background-image: url("../assets/img/icons/icon-blizzard.svg");
+}
+
+.icon--metorite {
+  background-image: url("../assets/img/icons/icon-metorite.svg");
+}
+
+.icon--wind {
+  background-image: url("../assets/img/icons/icon-wind.svg");
+}
+
+.input-wrapper {
+  display: flex;
+}
+
+.input-wrapper input {
+  border-radius: 2px;
+}
+
+.input-wrapper--search input {
+  width: 254px;
+  padding: 8px 20px;
+
+  color: var(--color-text-input);
+
+  background-color: var(--color-input-base);
+  border: none;
+  outline: none;
+
+  transition: background-color var(--transition-base);
+}
+
+.input-wrapper--search input:hover {
+  background-color: var(--color-input-hover);
+}
+
+.input-wrapper--search input:focus::placeholder {
+  opacity: 0;
+}
+
+.input-wrapper--search input:focus::-ms-input-placeholder {
+  opacity: 0;
+}
+
+.input-wrapper--search input:focus {
+  background-color: var(--color-input-focus);
+}
+
+.input-wrapper--search input::placeholder {
+  color: var(--color-text-placeholder);
+
+  transition: opacity var(--transition-base);
+}
+
+.input-wrapper--search input::-ms-input-placeholder {
+  color: var(--color-text-placeholder);
+
+  transition: opacity var(--transition-base);
+}
+
+.input-wrapper--radio label,
+.input-wrapper--checkbox label {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+
+  background-color: var(--color-label-base);
+  border-radius: 2px;
+  cursor: pointer;
+
+  transition: background-color var(--transition-base),
+    box-shadow var(--transition-base);
+
+  user-select: none;
+}
+
+.input-wrapper--radio label {
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.input-wrapper--radio label:hover,
+.input-wrapper--checkbox label:hover {
+  background-color: var(--color-label-hover);
+  box-shadow: 0 4px 10px rgba(11, 23, 78, 0.5);
+}
+
+.input-wrapper--radio label:active,
+.input-wrapper--checkbox label:active {
+  background-color: var(--color-label-active);
+}
+
+.input-wrapper--radio label > span,
+.input-wrapper--checkbox label > span {
+  transition: opacity var(--transition-base);
+}
+
+.input-wrapper--radio label:active > span,
+.input-wrapper--checkbox label:active > span {
+  opacity: 0.3;
+}
+
+.input-wrapper--radio input,
+.input-wrapper--checkbox input {
+  position: absolute;
+
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+
+  clip: rect(0 0 0 0);
+}
+
+.input-wrapper--radio input:focus + label,
+.input-wrapper--checkbox input:focus + label {
+  background-color: var(--color-label-hover);
+}
+
+.input-wrapper--radio input:disabled + label,
+.input-wrapper--checkbox input:disabled + label {
+  opacity: 0.5;
+
+  pointer-events: none;
+}
+
+.input-wrapper--radio input:checked + label,
+.input-wrapper--checkbox input:checked + label {
+  background-color: var(--color-label-active);
+  box-shadow: none;
+}
+
+.input-wrapper--radio input:checked + label:hover,
+.input-wrapper--checkbox input:checked + label:hover {
+  background-color: var(--color-label-hover-checked);
+}
+
+.input-wrapper--radio input:checked:focus + label,
+.input-wrapper--checkbox input:checked:focus + label {
+  background-color: var(--color-label-hover-checked);
+}
+</style>
--- src/components/LeftSideBar.vue
+++ src/components/LeftSideBar.vue
@@ -66,3 +66,278 @@ export default {
   name: "LeftSideBar",
 };
 </script>
+
+<style lang="scss" scoped>
+.weather-content__result {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  padding: 2px 4px 0 4px;
+}
+
+.weather-content__small-cards,
+.weather-content__big-cards {
+  width: calc(50% - 2px);
+  height: 100%;
+  padding-bottom: 100px;
+  overflow-y: auto;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.weather-content__small-cards::-webkit-scrollbar,
+.weather-content__big-cards::-webkit-scrollbar {
+  display: none;
+}
+
+.weather-content__small-cards > div,
+.weather-content__big-cards > div {
+  margin-bottom: 2px;
+}
+
+.weather-content__small-cards > div:last-child,
+.weather-content__big-cards > div:last-child {
+  margin-bottom: 80px;
+}
+
+.weather-content__big-cards {
+  position: relative;
+  z-index: 1;
+}
+
+.weather-content__help {
+  position: absolute;
+  top: 56px;
+  left: 0;
+  z-index: -1;
+
+  width: 100%;
+  padding-right: 65px;
+  padding-left: 65px;
+
+  line-height: 32px;
+  text-align: center;
+  color: var(--color-text-help);
+}
+
+.weather-content__sort .sort-form__group:nth-child(1) {
+  width: 82px;
+}
+
+.weather-content__sort .sort-form__group:nth-child(1) div:nth-child(2n) {
+  margin-right: 0;
+}
+
+.weather-content__sort .sort-form__group:nth-child(3) {
+  width: 292px;
+}
+
+.weather-content__sort .sort-form__group:nth-child(3) div:nth-child(7n) {
+  margin-right: 0;
+}
+
+.small-card {
+  display: flex;
+  align-items: center;
+  width: 350px;
+  padding: 16px 20px;
+
+  background-color: var(--color-light-grey);
+  border-radius: 2px;
+  outline: none;
+  cursor: pointer;
+
+  transition: background-color var(--transition-base),
+    filter var(--transition-base);
+}
+
+.small-card:hover {
+  background-color: var(--color-blue-light);
+}
+
+.small-card:hover .small-card__city,
+.small-card:hover .small-card__temperature {
+  color: var(--color-text-white);
+}
+
+.small-card__city {
+  flex-grow: 1;
+  margin-right: 20px;
+
+  transition: color var(--transition-base);
+}
+
+.small-card__temperature {
+  margin-right: 20px;
+
+  font-size: 24px;
+  line-height: 24px;
+  letter-spacing: -0.02em;
+
+  transition: color var(--transition-base);
+}
+
+.small-card:active {
+  filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
+}
+
+.small-card--shadow {
+  pointer-events: none;
+  background-color: var(--color-shadow-main);
+}
+
+.small-card--shadow .small-card__city,
+.small-card--shadow .small-card__temperature {
+  color: var(--color-text-white);
+}
+
+.small-card--empty {
+  pointer-events: none;
+  min-height: 56px;
+  background-color: rgba(247, 248, 255, 0.3);
+  border: 2px dashed var(--color-border);
+}
+
+.big-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 350px;
+  min-height: 172px;
+
+  border-radius: 2px;
+  outline: none;
+  cursor: pointer;
+
+  transition: filter var(--transition-base);
+}
+
+.big-card__header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 20px 20px 14px 20px;
+
+  background-color: var(--color-white);
+
+  transition: background-color var(--transition-base);
+}
+
+.big-card__city {
+  margin-left: 20px;
+
+  font-size: 30px;
+  line-height: 36px;
+
+  transition: color var(--transition-base);
+}
+
+.big-card__content {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 16px 24px 24px 20px;
+
+  background-color: var(--color-light-grey);
+
+  transition: background-color var(--transition-base);
+}
+
+.big-card__weather-conditions {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-bottom: -2px;
+}
+
+.big-card__weather-conditions span {
+  margin-right: 10px;
+  margin-bottom: 2px;
+}
+
+.big-card__weather-conditions span:last-child {
+  margin-right: 0;
+}
+
+.big-card__wind {
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+}
+
+.big-card__wind-info {
+  margin-left: 10px;
+
+  color: var(--color-text-light-blue);
+
+  opacity: 0.8;
+
+  transition: color var(--transition-base);
+}
+
+.big-card__temperature {
+  margin-left: auto;
+
+  font-size: 64px;
+  line-height: 62px;
+  letter-spacing: -0.05em;
+
+  opacity: 0.8;
+
+  transition: color var(--transition-base);
+}
+
+.big-card:hover .big-card__city {
+  color: var(--color-text-white);
+}
+
+.big-card:hover .big-card__temperature,
+.big-card:hover .big-card__wind-info {
+  color: var(--color-text-white);
+}
+
+.big-card:hover .big-card__header {
+  background-color: var(--color-blue-light);
+}
+
+.big-card:hover .big-card__content {
+  background-color: var(--color-blue-main);
+}
+
+.big-card:active {
+  filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
+}
+
+.big-card--shadow {
+  pointer-events: none;
+  opacity: 0.3;
+  background-color: var(--color-blue-light);
+}
+
+.big-card--shadow .big-card__header {
+  background-color: var(--color-transparent);
+}
+
+.big-card--shadow .big-card__content {
+  background-color: var(--color-blue-deep);
+}
+
+.big-card--shadow .big-card__city,
+.big-card--shadow .big-card__temperature,
+.big-card--shadow .big-card__wind-info {
+  color: var(--color-text-white);
+}
+
+.big-card--empty {
+  pointer-events: none;
+  min-height: 172px;
+  background-color: rgba(247, 248, 255, 0.3);
+  border: 2px dashed var(--color-border);
+}
+
+.big-card--empty.hidden-block {
+  display: none;
+}
+</style>
