5eedb9d17df490742a6fb64ab8491c0a44a908e0
Movsumlu Tabriz <movsumlutabriz@gmail.com>
2021-07-02 17:33:26 +0300

    refactoring checkbox in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -41,99 +41,17 @@
             name="city-search"
             placeholder="Название города"
           />
-          <label for="search" aria-label="Поиск городов"></label>
         </div>
       </div>
       <div class="sort-form__group">
         <div
+          v-for="condition in weatherConditions"
+          :key="condition.value"
           class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
         >
-          <input
-            id="rainy"
-            name="weather-conditions"
-            type="checkbox"
-            value="rainy"
-          />
-          <label aria-label="Дождливо" for="rainy">
-            <span class="icon icon--rainy"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="sunny"
-            name="weather-conditions"
-            type="checkbox"
-            value="sunny"
-          />
-          <label aria-label="Солнечно" for="sunny">
-            <span class="icon icon--sunny"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="cloudy"
-            name="weather-conditions"
-            type="checkbox"
-            value="cloudy"
-          />
-          <label aria-label="Облачно" for="cloudy">
-            <span class="icon icon--cloudy"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="snowy"
-            name="weather-conditions"
-            type="checkbox"
-            value="snowy"
-          />
-          <label aria-label="Снежно" for="snowy">
-            <span class="icon icon--snowy"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="stormy"
-            name="weather-conditions"
-            type="checkbox"
-            value="stormy"
-          />
-          <label aria-label="Торнадо" for="stormy">
-            <span class="icon icon--stormy"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="blizzard"
-            name="weather-conditions"
-            type="checkbox"
-            value="blizzard"
-          />
-          <label aria-label="Гроза" for="blizzard">
-            <span class="icon icon--blizzard"></span>
-          </label>
-        </div>
-        <div
-          class="sort-form__input-wrapper input-wrapper input-wrapper--checkbox"
-        >
-          <input
-            id="metorite"
-            name="weather-conditions"
-            type="checkbox"
-            value="metorite"
-          />
-          <label aria-label="Метеоритный дождь" for="metorite">
-            <span class="icon icon--metorite"></span>
+          <input type="checkbox" :value="condition.value" />
+          <label>
+            <span :class="['icon', condition['icon']]"></span>
           </label>
         </div>
       </div>
@@ -149,6 +67,36 @@ export default {
   data() {
     return {
       seachCityName: "",
+      weatherConditions: [
+        {
+          value: "rainy",
+          icon: "icon--rainy",
+        },
+        {
+          value: "sunny",
+          icon: "icon--sunny",
+        },
+        {
+          value: "cloudy",
+          icon: "icon--cloudy",
+        },
+        {
+          value: "snowy",
+          icon: "icon--snowy",
+        },
+        {
+          value: "stormy",
+          icon: "icon--stormy",
+        },
+        {
+          value: "blizzard",
+          icon: "icon--blizzard",
+        },
+        {
+          value: "metorite",
+          icon: "icon--metorite",
+        },
+      ],
     };
   },
   watch: {
