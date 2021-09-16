306f5331a041ca8423aec9ad8e3e0e0d97474976
Movsumlu Tabriz <movsumlutabriz@gmail.com>
2021-09-16 12:57:57 +0300

    update css-styles for Cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -31,7 +31,7 @@
     >
       <div
         v-if="!showLoader && !showBigEmptyCard && !filteredBigCards.length"
-        class="cards__help"
+        class="cards-container__help"
       >
         Перетащите сюда города, <br />погода в которых вам интересна
       </div>
@@ -222,215 +222,213 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.cards {
-  &-container {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    height: 100%;
-    padding: 2px 4px 0 4px;
+.cards-container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  padding: 2px 4px 0 4px;
 
-    &__big-cards {
-      position: relative;
-      z-index: 1;
-    }
+  &__big-cards {
+    position: relative;
+    z-index: 1;
+  }
 
-    &__sort {
-      .sort-form__group:nth-child(3) {
-        width: 292px;
-      }
-    }
-
-    .small-card {
-      display: flex;
-      align-items: center;
-      width: 350px;
-      padding: 16px 20px;
-      background-color: var(--color-light-grey);
-      border-radius: 2px;
-      outline: none;
-      transition: background-color var(--transition-base),
-        filter var(--transition-base);
-
-      span {
-        user-select: none;
-      }
-
-      &:hover {
-        cursor: grab;
-        background-color: var(--color-blue-light);
-        .small-card__city,
-        .small-card__temperature {
-          color: var(--color-text-white);
-        }
-      }
-
-      &__city {
-        flex-grow: 1;
-        margin-right: 20px;
-        transition: color var(--transition-base);
-      }
-
-      &__temperature {
-        margin-right: 20px;
-        font-size: 24px;
-        line-height: 24px;
-        letter-spacing: -0.02em;
-        transition: color var(--transition-base);
-      }
-
-      &:active {
-        cursor: grabbing;
-        filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
-      }
-
-      &--shadow {
-        pointer-events: none;
-        background-color: var(--color-shadow-main);
-      }
-
-      &--empty {
-        pointer-events: none;
-        min-height: 56px;
-        background-color: rgba(247, 248, 255, 0.3);
-        border: 2px dashed var(--color-border);
-      }
-    }
-
-    .big-card {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 350px;
-      min-height: 172px;
-      border-radius: 2px;
-      outline: none;
-      transition: filter var(--transition-base);
-
-      &__header {
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 20px 20px 14px 20px;
-        background-color: var(--color-text-white);
-        transition: background-color var(--transition-base);
-      }
-
-      &__city {
-        margin-left: 20px;
-        font-size: 30px;
-        line-height: 36px;
-        transition: color var(--transition-base);
-      }
-
-      &__wind {
-        display: flex;
-        align-items: center;
-        margin-top: 12px;
-      }
-
-      &__temperature {
-        margin-left: auto;
-        font-size: 64px;
-        line-height: 62px;
-        letter-spacing: -0.05em;
-        opacity: 0.8;
-        transition: color var(--transition-base);
-      }
-
-      &--shadow {
-        pointer-events: none;
-        opacity: 0.3;
-        background-color: var(--color-blue-light);
-      }
-
-      &__wind-info {
-        margin-left: 10px;
-        color: var(--color-text-light-blue);
-        opacity: 0.8;
-        transition: color var(--transition-base);
-      }
-
-      &__weather-conditions {
-        display: inline-block;
-
-        span {
-          margin-right: 10px;
-          &:first-child {
-            margin-left: 2px;
-          }
-        }
-      }
-
-      &--empty {
-        pointer-events: none;
-        min-height: 172px;
-        background-color: rgba(247, 248, 255, 0.3);
-        border: 2px dashed var(--color-border);
-      }
-
-      &__content {
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 16px 24px 24px 20px;
-        background-color: var(--color-light-grey);
-        transition: background-color var(--transition-base);
-      }
-
-      &:hover {
-        cursor: grab;
-        background-color: var(--color-blue-light);
-
-        .big-card__city,
-        .big-card__temperature,
-        .big-card__wind-info {
-          color: var(--color-text-white);
-        }
-
-        .big-card {
-          &__header {
-            background-color: var(--color-blue-light);
-          }
-
-          &__content {
-            background-color: var(--color-blue-main);
-          }
-        }
-      }
-
-      &:active {
-        cursor: grabbing;
-        filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
-      }
-    }
-
-    .small-card__container,
-    .big-card__container {
-      width: calc(50% - 2px);
-      height: 100%;
-      padding-bottom: 100px;
-      overflow-y: auto;
-      -ms-overflow-style: none;
-      scrollbar-width: none;
-
-      > div {
-        margin-bottom: 2px;
-        &:last-child {
-          margin-bottom: 80px;
-        }
-      }
-
-      &::-webkit-scrollbar,
-      &::-webkit-scrollbar {
-        display: none;
-      }
+  &__sort {
+    .sort-form__group:nth-child(3) {
+      width: 292px;
     }
   }
 
   &__help {
     text-align: center;
     margin-top: 60px;
+  }
+
+  .small-card {
+    display: flex;
+    align-items: center;
+    width: 350px;
+    padding: 16px 20px;
+    background-color: var(--color-light-grey);
+    border-radius: 2px;
+    outline: none;
+    transition: background-color var(--transition-base),
+      filter var(--transition-base);
+
+    span {
+      user-select: none;
+    }
+
+    &:hover {
+      cursor: grab;
+      background-color: var(--color-blue-light);
+      .small-card__city,
+      .small-card__temperature {
+        color: var(--color-text-white);
+      }
+    }
+
+    &__city {
+      flex-grow: 1;
+      margin-right: 20px;
+      transition: color var(--transition-base);
+    }
+
+    &__temperature {
+      margin-right: 20px;
+      font-size: 24px;
+      line-height: 24px;
+      letter-spacing: -0.02em;
+      transition: color var(--transition-base);
+    }
+
+    &:active {
+      cursor: grabbing;
+      filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
+    }
+
+    &--shadow {
+      pointer-events: none;
+      background-color: var(--color-shadow-main);
+    }
+
+    &--empty {
+      pointer-events: none;
+      min-height: 56px;
+      background-color: rgba(247, 248, 255, 0.3);
+      border: 2px dashed var(--color-border);
+    }
+  }
+
+  .big-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 350px;
+    min-height: 172px;
+    border-radius: 2px;
+    outline: none;
+    transition: filter var(--transition-base);
+
+    &__header {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding: 20px 20px 14px 20px;
+      background-color: var(--color-text-white);
+      transition: background-color var(--transition-base);
+    }
+
+    &__city {
+      margin-left: 20px;
+      font-size: 30px;
+      line-height: 36px;
+      transition: color var(--transition-base);
+    }
+
+    &__wind {
+      display: flex;
+      align-items: center;
+      margin-top: 12px;
+    }
+
+    &__temperature {
+      margin-left: auto;
+      font-size: 64px;
+      line-height: 62px;
+      letter-spacing: -0.05em;
+      opacity: 0.8;
+      transition: color var(--transition-base);
+    }
+
+    &--shadow {
+      pointer-events: none;
+      opacity: 0.3;
+      background-color: var(--color-blue-light);
+    }
+
+    &__wind-info {
+      margin-left: 10px;
+      color: var(--color-text-light-blue);
+      opacity: 0.8;
+      transition: color var(--transition-base);
+    }
+
+    &__weather-conditions {
+      display: inline-block;
+
+      span {
+        margin-right: 10px;
+        &:first-child {
+          margin-left: 2px;
+        }
+      }
+    }
+
+    &--empty {
+      pointer-events: none;
+      min-height: 172px;
+      background-color: rgba(247, 248, 255, 0.3);
+      border: 2px dashed var(--color-border);
+    }
+
+    &__content {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding: 16px 24px 24px 20px;
+      background-color: var(--color-light-grey);
+      transition: background-color var(--transition-base);
+    }
+
+    &:hover {
+      cursor: grab;
+      background-color: var(--color-blue-light);
+
+      .big-card__city,
+      .big-card__temperature,
+      .big-card__wind-info {
+        color: var(--color-text-white);
+      }
+
+      .big-card {
+        &__header {
+          background-color: var(--color-blue-light);
+        }
+
+        &__content {
+          background-color: var(--color-blue-main);
+        }
+      }
+    }
+
+    &:active {
+      cursor: grabbing;
+      filter: drop-shadow(0 8px 20px rgba(11, 23, 78, 0.5));
+    }
+  }
+
+  .small-card__container,
+  .big-card__container {
+    width: calc(50% - 2px);
+    height: 100%;
+    padding-bottom: 100px;
+    overflow-y: auto;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    > div {
+      margin-bottom: 2px;
+      &:last-child {
+        margin-bottom: 80px;
+      }
+    }
+
+    &::-webkit-scrollbar,
+    &::-webkit-scrollbar {
+      display: none;
+    }
   }
 }
 </style>
